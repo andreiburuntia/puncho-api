602ecdda8fb3fd31237d66c72b89dd78cc30e8fc
andrei <andreiburuntia@gmail.com>
2020-07-09 17:29:05 +0300

    workout schema and class fixup

--- models/workout.py
+++ models/workout.py
@@ -9,13 +9,11 @@ class Workout(db.Model):
     __tablename__ = 'workouts'
     
     id = Column(Integer, primary_key=True)
-    description = Column(String(250))
     name = Column(String(30))
+    description = Column(String(250))
     start_time = Column(DateTime, default=datetime.datetime.utcnow)
-    end_time = Column(DateTime, default=datetime.datetime.utcnow)
     
-    def __init__(self, name, description, start_time, end_time):
+    def __init__(self, name, description, start_time):
         self.name = name
         self.description = description
         self.start_time = start_time
-        self.end_time = end_time
