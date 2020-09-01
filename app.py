9036f0ffa454fbe6cee1739741b90f501295abb2
andreiburuntia@gmail.com <andreiburuntia@gmail.com>
2020-09-01 19:54:29 +0300

    moved params to body for bag link

--- app.py
+++ app.py
@@ -427,11 +427,13 @@ def get_bookings_for_user(user_id):
 # Link Bag
 @app.route('/link_bag', methods=['POST'])
 def link_bag():
-    user_id = request.args.get('user_id')
-    bag_id = request.args.get('bag_id')
-    bag_map[bag_id] = user_id
-    return 'OK'
-    
+    user_id = request.json['user_id']
+    bag_id = request.json['bag_id']
+    if bag_map[bag_id] == "":
+        bag_map[bag_id] = user_id
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    else
+        return json.dumps({'success':False}), 409, {'ContentType':'application/json'} 
 
 # ----------- END SESSION ------------
 @app.route('/end_session/<user_id>', methods=['POST'])
