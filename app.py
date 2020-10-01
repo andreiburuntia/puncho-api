f3f5e72c7671de19b0755cf47415e92fb8704a87
andrei <andreiburuntia@gmail.com>
2020-10-01 14:42:38 +0300

    receptie

--- app.py
+++ app.py
@@ -12,9 +12,6 @@ import random
 import platform
 import sys
 
-from authlib.integrations.flask_client import OAuth
-from auth_decorator import login_required
-
 # Init app
 app = Flask(__name__)
 cors = CORS(app)
@@ -27,23 +24,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin123@database
 app.secret_key = os.getenv("APP_SECRET_KEY")
 app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
-
-# oAuth Setup
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id=os.getenv("GOOGLE_CLIENT_ID"),
-    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-    client_kwargs={'scope': 'openid email profile'},
-)
-
-    
+ 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Init db
