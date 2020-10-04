0a23b24f737362b9b3693d12425d4852caabc6b8
andrei <andreiburuntia@gmail.com>
2020-10-04 15:23:15 +0300

    apple signin

--- app.py
+++ app.py
@@ -251,10 +251,8 @@ def apple_sign_in_clinet():
             break
     if user == {}:
         user = User(firstname, lastname, email, 'apple')
-    else:
-        return user_schema.jsonify(user)
-
-    return 'ok'
+    
+    return user_schema.jsonify(user)
 
 marius_jwt = 'eyJraWQiOiJCRkpRUFY2WEdYIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJDUVJRNTVVSzZWIiwiaWF0IjoxNjAxNzI5NTc2LCJleHAiOjE2MTcyODE1NzYsImF1ZCI6Imh0dHBzOi8vYXBwbGVpZC5hcHBsZS5jb20iLCJzdWIiOiJjb20ubGVnZW5kLmJveGluZy5jbGllbnQifQ.GNBAyLgtQ3eO4fJiqjeoIB7iQMlbTzpG1FOdjA5nwTfZJ8NMNlKnjbjVqKKfWrovx4_r6o-h8bTtP2NfPQGYLQ'
 
