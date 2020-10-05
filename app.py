8cdab7f0d4913522a931bb0c08ad2811cbbf4fea
andrei <andreiburuntia@gmail.com>
2020-10-05 17:25:30 +0300

    proiector

--- app.py
+++ app.py
@@ -634,9 +634,9 @@ def proiector():
         offset = '0'
         if i<9:
             offset = '00'
-        print(offset + str(obj['bag_id']))
+        #print(offset + str(obj['bag_id']))
         if offset + str(obj['bag_id']) in used_bags:
-            print(obj['bag_id'])
+            #print(obj['bag_id'])
             usr = bag_map[offset + str(obj['bag_id'])]
             user_id = usr
             try:
