c4d4f41d2fdeab9e17faee272843beff2258873c
Buruntia <andrei.buruntia@cpp.canon>
2020-12-01 10:57:35 +0200

    adding stripe

--- app.py
+++ app.py
@@ -159,6 +159,27 @@ used_bags = []
 
 # ----------- STRIPE ----------
 
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+  session = stripe.checkout.Session.create(
+    payment_method_types=['card'],
+    line_items=[{
+      'price_data': {
+        'currency': 'usd',
+        'product_data': {
+          'name': 'T-shirt',
+        },
+        'unit_amount': 2000,
+      },
+      'quantity': 1,
+    }],
+    mode='payment',
+    success_url='https://example.com/success',
+    cancel_url='https://example.com/cancel',
+  )
+
+  return jsonify(id=session.id)
+
 @app.route('/stripe')
 def index():
     return render_template('stripe.html', key=stripe_keys['publishable_key'])
