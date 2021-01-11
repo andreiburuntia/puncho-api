fc9f8bbce09067e4d8cff857b296bb43d8fa1c92
Andrei Buruntia <andrei.buruntia@cpp.canon>
2021-01-11 15:02:48 +0200

    tokenz

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ def add_user():
     db.session.commit()
 
     
-    new_sub = Subscription(usr.id, datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(months=1), 1, 1)
+    new_sub = Subscription(usr.id, datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(days=30), 1, 1)
 
     db.session.add(new_sub)
     db.session.commit()
