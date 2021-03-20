61df6c542fbdf5e0061f8ac67f0981cc7a20bea3
boro <andreiburuntia@gmail.com>
2021-03-20 16:18:34 +0200

    update connected-users

--- app.py
+++ app.py
@@ -1071,7 +1071,7 @@ def connected_users():
     for k in bag_map:
         if bag_map[k] != "":
             usr = User.query.get(int(bag_map[k]))
-            tup = (k, user_schema.jsonify(usr).content)
+            tup = (k, user_schema.jsonify(usr).text)
             print(tup)
             users.append(tup)
         
