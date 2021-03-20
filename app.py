4cc0e779dd39cfebe9e03318678304d6807a6231
boro <andreiburuntia@gmail.com>
2021-03-20 16:39:43 +0200

    update connected-users

--- app.py
+++ app.py
@@ -1071,7 +1071,7 @@ def connected_users():
     for k in bag_map:
         if bag_map[k] != "":
             usr = User.query.get(int(bag_map[k]))
-            tup = (k, user_schema.dump(usr))
+            tup = (k, user_schema.dump(usr)[0])
             users.append(tup)
         
     return Response(json.dumps(users), mimetype='application/json')
